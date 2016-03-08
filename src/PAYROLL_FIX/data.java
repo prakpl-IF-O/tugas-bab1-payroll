34710cac105d99cabe17bc0bf21389ac069c2185
Muhammad Heryan Chaniago <muhammadheryan@ymail.com>
2016-03-09 03:10:06 +0700

    Deklarasi dan Inisialisasi data field

--- src/PAYROLL_FIX/data.java
+++ src/PAYROLL_FIX/data.java
@@ -1,14 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package PAYROLL_FIX;
-
-/**
- *
- * @author Heryan
- */
+import java.util.Scanner;
 public class data {
-    
+    int nik1=101; int nik2=102; int nik3=103;
+    String nam1="Paijo"; String nam2="Lulu"; String nam3="Lala";
+    double gaji1=500, gaji2=875, gaji3=1000;
+    double tun1=500, tun2=200, tun3=350;
+    double bon1=100, bon2=100, bon3=200;
+    double den1=100, den2=100, den3=125;
+    double bonus, denda;
+    int hk, lemburlibur;
+    double telat, telatt=0, lembur, lemburr=0;
 }
