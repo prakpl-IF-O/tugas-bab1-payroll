42c3e9806595147df9552900d2cb96547a44ac71
Muhammad Heryan Chaniago <muhammadheryan@ymail.com>
2016-03-09 03:20:59 +0700

    Instansi Objek objek dari kelas data

--- src/PAYROLL_FIX/MAIN.java
+++ src/PAYROLL_FIX/MAIN.java
@@ -3,5 +3,9 @@ import java.util.Scanner;
 public class MAIN {
     public static void main(String[] args) {
         Scanner masukan=new Scanner(System.in);
+        data d=new data();
+        data k1=new data();
+        data k2=new data();
+        data k3=new data();
     }
 }
