cc9a358bc47e7a18ef981115b5e747003c9a785c
Faza Abdi Perkasa <abdifaza@yahoo.com>
2016-03-08 20:43:57 +0700

    Pembuatan class dataKaryawan dan pembuatan array data

---
+++ src/payroll/dataKaryawan.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package payroll;
+
+/**
+ *
+ * @author Faza Abdi
+ */
+public class dataKaryawan {
+    private String data[][]={{"101","paijo","900","200","150","75"},{"102","lulu","875","200","100","100"},{"103","lala","1000","350","200","125"}};
+}
