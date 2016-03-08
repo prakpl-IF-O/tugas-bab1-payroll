f1c0170bc55287a939b9ef2c95e2164d5fb6f8fb
Reza Faridh Mashyuri <refamasfuma@gmail.com>
2016-03-08 18:59:25 +0700

    Membuat method set untuk memberikan nilainya setiap object

--- src/Tugas1.java
+++ src/Tugas1.java
@@ -6,17 +6,52 @@ public class Tugas1 {
     public double gaji,tunjangan,bonus,denda,jumlah;
     
     public static void main(String[]args){
-        Tugas1 karyawan_1 = new Tugas1();
-        Tugas1 karyawan_2 = new Tugas1();
-        Tugas1 karyawan_3 = new Tugas1();
-        
-            }
+        Tugas1 Ka = new Tugas1();      
+        Ka.setNik(101);
+        Ka.setNama("Paijo");
+        Ka.setGaji(900);
+        Ka.setTunjangan(200);
+        Ka.setBonus(150);
+        Ka.setDenda(75);
+        Tugas1 Kb = new Tugas1();
+        Kb.setNik(102);
+        Kb.setNama("Lulu");
+        Kb.setGaji(875);
+        Kb.setTunjangan(200);
+        Kb.setBonus(100);
+        Kb.setDenda(100);
+        Tugas1 Kc = new Tugas1();
+        Ka.setNik(103);
+        Ka.setNama("Lala");
+        Ka.setGaji(1000);
+        Ka.setTunjangan(350);
+        Ka.setBonus(200);
+        Ka.setDenda(125);
+                
+    
+    }
     public void setNik (int s){
         nik = s;
     }
     public void setNama(String s){
         nama = s;
     }
+    public void setGaji(double s){
+        gaji = s;
+    }
+    public void setTunjangan(double s){
+        tunjangan = s;
+    }
+    public void setBonus(double s){
+        bonus = s;
+    }
+    public void setDenda(double s){
+        denda = s;
+    }
+    public void Total(double s){
+        
+    }
+    
     
     
             
