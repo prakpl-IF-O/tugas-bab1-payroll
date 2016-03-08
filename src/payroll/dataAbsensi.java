bb71366d5bd117a90f27caa7ed76c7921225b12d
Faza Abdi Perkasa <abdifaza@yahoo.com>
2016-03-08 20:52:02 +0700

    Pembuatan class dataAbsensi dan array absensi

---
+++ src/payroll/dataAbsensi.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package payroll;
+
+/**
+ *
+ * @author Faza Abdi
+ */
+public class dataAbsensi {
+    private String absen[][]={{"101","2016-01-15","08:00","17:00"},{"101","2016-01-16","08:00","18:30"},{"102","2016-01-15","08:00","15:00"},{"103","2016-01-15","08:00","17:00"}};
+}
