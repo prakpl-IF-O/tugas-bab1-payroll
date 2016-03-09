0c2f4aa3ba5c3786a038b2b30031d7c72879332e
dwiyan <dwiyan774@gmail.com>
2016-03-09 12:10:46 +0700

    mengambil fungsi java.time.LocalTime, java.time.format.DateTimeFormatter, java.text.ParseExseption, java.time.localDate;
    Menambahkan waktu menggunakan LocalTime.

--- src/Github/JadwalKaryawan.java
+++ src/Github/JadwalKaryawan.java
@@ -7,17 +7,21 @@ package Github;
 
 import java.util.Date;
 import java.text.SimpleDateFormat;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.text.ParseException;
+import java.time.LocalDate;
 
 /**
  *
  * @author dwiyan
  */
 public class JadwalKaryawan {
-
+    
     String data[][] = new String[4][4];
-
+    
     public void setJadwal() {
-
+        
         Date paijo = new Date(116, 0, 15);
         Date paijo_ = new Date(116, 0, 16);
         Date lulu = new Date(116, 0, 15);
@@ -26,25 +30,31 @@ public class JadwalKaryawan {
         SimpleDateFormat lulu1 = new SimpleDateFormat("EEEE");
         SimpleDateFormat lala1 = new SimpleDateFormat("EEEE");
         SimpleDateFormat hari = new SimpleDateFormat("yyyy-MM-dd");
-
+        LocalTime paijoMasuk = LocalTime.of(8, 0);
+        SimpleDateFormat a = new SimpleDateFormat("HH-mm");
+        LocalTime paijo1Masuk = LocalTime.of(8, 0);
+        LocalTime luluMasuk = LocalTime.of(8, 0);
+        LocalTime lalaMasuk = LocalTime.of(8, 0);
+        
         data[0][0] = "101";
         data[0][1] = hari.format(paijo);
+        //data[0][3] = a.format(paijoMasuk);
         data[1][0] = "101";
         data[1][1] = hari.format(paijo_);
         data[2][0] = "102";
         data[2][1] = hari.format(lulu);
         data[3][0] = "102";
         data[3][1] = hari.format(lala);
-        
+        // data[4]
 
     }
-
-    public void display(){
+    
+    public void display() {
         setJadwal();
         System.out.println("nik,tanggal,jam_masuk,jam_pulang");
-        for (int x =0; x<data.length; x++){
-            for (int y=0; y<data[1].length;y++) {
-                System.out.print(data[x][y]+",");
+        for (int x = 0; x < data.length; x++) {
+            for (int y = 0; y < data[1].length; y++) {
+                System.out.print(data[x][y] + ",");
             }
             System.out.println("");
         }
