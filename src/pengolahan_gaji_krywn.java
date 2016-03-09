59b17b50c42c0d166e7a896dcbbe76d978bc343a
Enggar Pratama Kurniawan <itsenggar@gmail.com>
2016-03-09 12:30:44 +0700

    memasukkan deklarasi nama nik gaji

---
+++ src/pengolahan_gaji_krywn.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Enggar Pratama
+ */
+public class pengolahan_gaji_krywn {
+    private int NIK;
+    private int nama;
+    private int gaji;
+    private int bonus;
+    private int denda;
+    private int hasil_gaji;
+    
+}
