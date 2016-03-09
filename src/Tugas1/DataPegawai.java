0c0acc123131ddfe4f13bf8a843e8add534f83a6
Muhammad Aditya Pramudita <adityapramudita23@gmail.com>
2016-03-09 12:43:56 +0700

    Menambahkan Objek Class baru dan Membuat ketentuan untuk menetukan Jammasuk karyawan serta menampilkan data yang akan Keluar sesuai ketentuan karyawan masuk.

--- src/Tugas1/DataPegawai.java
+++ src/Tugas1/DataPegawai.java
@@ -12,12 +12,23 @@ package Tugas1;
 public class DataPegawai {
     private String nama;
     private int nik;
-    private int gaji;
-    private int bonus;
-    private int denda;
-    private int tunjangan;
+    public double gaji;
+    public double tunjangan;
+    public double bonus;
+    public double denda;
+    public double jumdenda;
+    public double jumlah;
+    public int masuk = 8;
+    public int keluar = 17;
+    double [] bon = new double [2];
+    public String [] tanggal = new String[2];
+    double []masukjam = new double [2];
+    double []bolosjam = new double [2];
+    double []keluarjam = new double [2];
+    double []telatjam = new double [2];
+    double []jum = new double [2];
+   
     
-
     public DataPegawai (int ni, String na, int ga,int t, int b, int de) {
         nik = ni;
         nama = na;
@@ -30,6 +41,43 @@ public class DataPegawai {
 
     public DataPegawai() {
         
+    }
+    public void ketentuanJammasuk1 (double p,double q,double r) {
+        if (p>8){
+            telatjam[0] = (double) (p-8);
+        }
+        if (q<17){
+            bolosjam[0] =(double) (17-q);
+      } else {
+            bon [0]= (q-17)*r;
+        }
+    }
+    public void ketentuanJammasuk2 (double p,double x,double r) {
+     
+        if (p>8){
+            telatjam[1] = (double) (p-8);
+        }
+        if (x<17){
+            bolosjam[1] =(double) (17-x);
+      } else {
+            bon [1] = (x-17)*r;
+        }
+    }
+    public void jumlah (){
+        if (tanggal[1]==null) {
+        jumlah = (gaji + tunjangan)+ bon[0] + bon[1] - jumdenda;
+        }else{
+         jumlah = (gaji+tunjangan)*2+ bon[0] + bon[1] - jumdenda;   
+        } 
+    }
+    public void jumDenda1(double z){
+    jum[0] = (telatjam[0]+bolosjam[0])*z;  
+    }
+    public void jumDenda2(double z){
+    jum[1] = (telatjam[1]+bolosjam[1])*z;  
+    }
+    public void jumlahdenda(){
+        jumdenda = jum[0]+jum[1];
     }
 
     public void setNik(int ni) {
@@ -63,21 +111,34 @@ public class DataPegawai {
     public String getNama() {
         return nama;
     }
-    public int getDenda() {
+    public double getDenda() {
         return denda;
     }
-    public int getTunjangan() {
+    public double getTunjangan() {
         return tunjangan;
     }
 
-    public int getGaji() {
+    public double getGaji() {
         return gaji;
     }
 
-    public int getBonus() {
+    public double getBonus() {
         return bonus;
     }
+    public void TampilkanData() {
+        System.out.println("NIK : " +nik);
+        System.out.println("Nama :" +nama);
+        System.out.println("Gaji : Rp. " +gaji +"00");
+        System.out.println("Tunjangan : Rp. " +tunjangan +"00");
+        System.out.println("Hari Ke 1 : " +tanggal[0]);
+        System.out.println("Bonus 1 : " +bon[0] +"00");
+        System.out.println("Denda 1 : " +jum[0] +"00");
+        System.out.println("Hari Ke 2 : " +tanggal[1]);
+        System.out.println("Bonus 2 : " +bon[1] +"00");
+        System.out.println("Denda 2 : " +jum[1] +"00");
+        System.out.println("===================== +");
+        System.out.printf("%-10s : Rp. %.3f\n\n","Jumlah gaji yang Di dapat",jumlah);    
 
-    
+    }
 }   
 
