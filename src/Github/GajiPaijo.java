ee3ab99216d79fb5e22787c7f871fb628248c782
dwiyan <dwiyan774@gmail.com>
2016-03-09 12:36:47 +0700

    Membuat kelas baru

---
+++ src/Github/GajiPaijo.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Github;
+
+/**
+ *
+ * @author dwiyan
+ */
+public class GajiPaijo {
+   public void jo(){
+    JadwalKaryawan m1 = new JadwalKaryawan();
+   
+   }
+}
