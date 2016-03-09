f5a50527a5403b9004bbdc61382f69a9d2346373
Faza Abdi Perkasa <abdifaza@yahoo.com>
2016-03-09 18:34:17 +0700

    ubah dataAbsensi dan menambahkan constructor, serta method getter

--- src/payroll/dataAbsensi.java
+++ src/payroll/dataAbsensi.java
@@ -10,5 +10,31 @@ package payroll;
  * @author Faza Abdi
  */
 public class dataAbsensi {
-    private String absen[][]={{"101","2016-01-15","08:00","17:00"},{"101","2016-01-16","08:00","18:30"},{"102","2016-01-15","08:00","15:00"},{"103","2016-01-15","08:00","17:00"}};
+
+    private int nik;
+    private double jam_masuk, jam_pulang;
+    private String tanggal;
+
+    public dataAbsensi(int a, String b, double c, double d) {
+        nik = a;
+        tanggal = b;
+        jam_masuk = c;
+        jam_pulang = d;
+    }
+
+    public int getNik() {
+        return nik;
+    }
+
+    public double getJam_masuk() {
+        return jam_masuk;
+    }
+
+    public double getJam_pulang() {
+        return jam_pulang;
+    }
+
+    public String getTanggal() {
+        return tanggal;
+    }
 }
