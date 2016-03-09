0960a292e00527679ae545b5909ff66fefe820fa
dwiyan <dwiyan774@gmail.com>
2016-03-09 13:08:02 +0700

    Membuat method untuk menghitung denda dan bonus

--- src/Github/HitungGaji.java
+++ src/Github/HitungGaji.java
@@ -5,10 +5,30 @@
  */
 package Github;
 
+import java.time.LocalTime;
+import java.text.SimpleDateFormat;
+
 /**
  *
  * @author dwiyan
  */
 public class HitungGaji {
-    
+
+    public void hitung() {
+        double gaji = 0;
+        JadwalKaryawan k1 = new JadwalKaryawan();
+
+        k1.setJadwal();
+        String a = "00:00";
+        String b = "01:00";
+        LocalTime a1 = LocalTime.parse(a);
+        LocalTime b1 = LocalTime.parse(b);
+
+        
+    }
+
+    public double bonus(double b) {
+        double banyakBonus = 0;
+        return banyakBonus;
+    }
 }
