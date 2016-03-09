e5d73d83d6559dbe9f6aaed127ee37d62bb4b822
Jerry Kurnia Sandy <jerrykurnias@gmail.com>
2016-03-09 20:37:00 +0700

    Penambahan Hari dan Gaji

--- src/Git.java
+++ src/Git.java
@@ -2,7 +2,10 @@ import java.util.Calendar;
 public class Git {  
 private int NIKKaryawan;
 public static String NamaKaryawan;
-private int  Gaji, Tunjangan, Bonus, Denda,Total_gaji, JamMasuk, JamKeluar;
+private int  Gaji, Tunjangan, Bonus, Denda,Total_gaji, JamMasuk, JamPulang, Calender;
+//campur
+boolean HARI=false;
+int yes, cc,no,MM;
 
 //Gaji
 int gaji ,co;
@@ -14,11 +17,19 @@ Calendar Tanggal_2 = Calendar.getInstance();
     public void setTotal_gaji(int o) {
         Total_gaji = (Gaji + Tunjangan + Bonus) - Denda;   
     }
+    public void days(int g){
+        if(g==2 || g==3){HARI=true;}
+        else if ((g-2)%7==0){
+        HARI=true;
+        }else if ((g-3)%7==0){
+        HARI=true;
+        }
+    }
     public void setJamMasuk(int p) {
         JamMasuk = p;
     }
-    public void setJamKeluar(int k) {
-        JamKeluar = k;
+    public void setJamPulang(int k) {
+        JamPulang = k;
     }
     public void setNIKKaryawan(int a) {
         NIKKaryawan = a;
@@ -26,8 +37,23 @@ Calendar Tanggal_2 = Calendar.getInstance();
     public static void setNamaKaryawan(String b) {
         NamaKaryawan = b;
     }
-    public void setGaji(int ca) {
+    public void setGaji(int ca,int cb,int cc) {
         gaji = ca ;
+        this.cc=cc;
+        yes=cb;
+        days(no);
+        boolean HARI=false;
+        if (HARI==true){
+        MM=cc*(sel-r);
+        if(msel>29){
+        MM=MM+cc/2;
+        }
+        }else if(sel>=17){
+        MM=cc*(sel-17);
+        if(msel>29){
+        MM+=cc/2;
+        }
+        }
     }
     public void setTunjangan(int d) {
         Tunjangan = d;
@@ -43,14 +69,14 @@ Calendar Tanggal_2 = Calendar.getInstance();
     }
     // Public Calender
     public void Calender (int q, int w , int e, int ra ,int t ,int y ,int u ){
-    Tanggal_1.set(q,w,e,r,t,0);
+    Tanggal_1.set(q,w,e,ra,t,0);
     Tanggal_2.set(q,w,e,y,u,0);
     sel=y;this.r=ra;
     msel=u;this.u=u;
-    
     }
     public void Display(){
         System.out.println(NIKKaryawan+"," + NamaKaryawan+","+ Gaji+"," + Tunjangan+"," + Bonus+","
         + Denda+"," + Total_gaji );
+        System.out.println(NIKKaryawan+"," + Calender + ","+ JamMasuk+","+ JamPulang+"," );
     }
 }
