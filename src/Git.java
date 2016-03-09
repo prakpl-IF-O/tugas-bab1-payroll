e2750c2a42e28f0f2b2ec306f6f3fce498c3a2d5
Jerry Kurnia Sandy <jerrykurnias@gmail.com>
2016-03-09 22:37:02 +0700

    Alhamdulillah Selesai

--- src/Git.java
+++ src/Git.java
@@ -2,20 +2,16 @@ import java.util.Calendar;
 public class Git {  
 private int NIKKaryawan;
 public static String NamaKaryawan;
-private int  Gaji, Tunjangan, Bonus, Denda,Total_gaji, JamMasuk, JamPulang, Calender;
-//campur
+private int  gaji, Tunjangan, Bonus, Denda,Total_gaji, JamMasuk, JamPulang, Calender;
+//campuran
 boolean HARI=false;
-int yes, cc,no,MM;
-
-//Gaji
-int gaji ,co;
+int cc,no,co;
 //Tanggal
-int Dn , nda, sel, msel, r, u, a, ww;
+int Dn ,sel, msel, r, u, a, ww, f;
 Calendar Tanggal_1 = Calendar.getInstance();
 Calendar Tanggal_2 = Calendar.getInstance();
-
-    public void setTotal_gaji(int o) {
-        Total_gaji = (Gaji + Tunjangan + Bonus) - Denda;   
+    public void Total_gaji() {
+        Total_gaji = (gaji + Tunjangan + Bonus) - Denda;   
     }
     public void days(int g){
         if(g==2 || g==3){HARI=true;}
@@ -40,18 +36,17 @@ Calendar Tanggal_2 = Calendar.getInstance();
     public void setGaji(int ca,int cb,int cc) {
         gaji = ca ;
         this.cc=cc;
-        yes=cb;
+        Tunjangan=cb;
         days(no);
-        boolean HARI=false;
         if (HARI==true){
-        MM=cc*(sel-r);
+        Bonus=cc*(sel-r);
         if(msel>29){
-        MM=MM+cc/2;
+        Bonus=Bonus+cc/2;
         }
         }else if(sel>=17){
-        MM=cc*(sel-17);
+        Bonus=cc*(sel-17);
         if(msel>29){
-        MM+=cc/2;
+        Bonus+=cc/2;
         }
         }
     }
@@ -63,20 +58,41 @@ Calendar Tanggal_2 = Calendar.getInstance();
     }
     
     public void setDenda(int f) {
-        if (Dn < 17){
-    }
-        nda=f;
+        this.f=f;
+        if(sel<=17){
+        Denda=f*(17-sel)+f*(r-8);
+        if (msel>29){
+        Denda+=f/2;
+        }
+        if(r>=8){
+        if(u>29){
+        Denda+=f/2;
+        }
+        }else{
+        Denda = 0;
+        }
+        }
     }
     // Public Calender
     public void Calender (int q, int w , int e, int ra ,int t ,int y ,int u ){
-    Tanggal_1.set(q,w,e,ra,t,0);
-    Tanggal_2.set(q,w,e,y,u,0);
+    Tanggal_1.set(q, w, e , ra, t, 0);
+    Tanggal_2.set(q, w, e, y, u, 0);
     sel=y;this.r=ra;
     msel=u;this.u=u;
     }
+    public void setcal(){
+        System.out.println("NIK"+ NIKKaryawan);
+        System.out.println("Mulai "+ Tanggal_1.getTime());
+        System.out.println("Stop "+ Tanggal_2.getTime());
+    }
     public void Display(){
-        System.out.println(NIKKaryawan+"," + NamaKaryawan+","+ Gaji+"," + Tunjangan+"," + Bonus+","
-        + Denda+"," + Total_gaji );
-        System.out.println(NIKKaryawan+"," + Calender + ","+ JamMasuk+","+ JamPulang+"," );
+        
+        System.out.println(NIKKaryawan+"," + NamaKaryawan+","+ gaji+"," + Tunjangan+"," + cc+","
+        + f);
+    }
+    public void Display2(){
+        
+        System.out.println(NIKKaryawan+"," + NamaKaryawan + ","+ gaji+","+ Tunjangan+","+ Bonus+","+Denda+","+Total_gaji);
     }
 }
+
--- src/MainGit.java
+++ src/MainGit.java
@@ -10,32 +10,48 @@ public class MainGit {
         System.out.println
         ("1. Paijo : 101\n2. Lulu  : 102\n3. Lala  : 103 ");
         System.out.println("");
-        System.out.println("NIK,Nama,Gaji,Tunjangan,Bonus,Denda,Total Gaji");
+        
         Karyawan1.setNIKKaryawan(101);
         Karyawan1.setNamaKaryawan("Paijo");
-        Karyawan1.setGaji(900);
-        Karyawan1.setTunjangan(200);
-        Karyawan1.setBonus(150);
+        Karyawan1.setGaji(900,200,150);
         Karyawan1.setDenda(75);
-        Karyawan1.Display();
+        Karyawan1.Calender(2016, 0, 15, 8 , 0, 17, 00);
+        Karyawan1.Total_gaji();
+       
         //System.out.println("");
         Git Karyawan2=new Git();
         Karyawan2.setNIKKaryawan(102);
         Karyawan2.setNamaKaryawan("Lulu");
-        Karyawan2.setGaji(875);
-        Karyawan2.setTunjangan(200);
-        Karyawan2.setBonus(100);
+        Karyawan2.setGaji(875,200,100);
         Karyawan2.setDenda(100);
-        Karyawan2.Display();
+        Karyawan1.Calender(2016, 0, 15, 8 , 0, 18, 30);
+        Karyawan2.Total_gaji();
+        
         //System.out.print
         Git Karyawan3=new Git();
         Karyawan3.setNIKKaryawan(103);
         Karyawan3.setNamaKaryawan("Lala");
-        Karyawan3.setGaji(1000);
-        Karyawan3.setTunjangan(350);
-        Karyawan3.setBonus(200);
+        Karyawan3.setGaji(1000,350,200);
         Karyawan3.setDenda(125);
+        Karyawan1.Calender(2016, 0, 15, 8 , 0, 17, 00);
+        Karyawan3.Total_gaji();
+        //Dis
+        System.out.println("NIK,Nama,Gaji,Tunjangan,Bonus,Denda");
+        Karyawan1.Display();
+        Karyawan2.Display();
         Karyawan3.Display();
+        System.out.println("");
+        System.out.println("NIK,Nama,Gaji,Tunjangan,Bonus,Denda,Total Gaji");
+        Karyawan1.Display2();
+        Karyawan2.Display2();
+        Karyawan3.Display2();
+        System.out.println("");
+        Karyawan1.setcal();
+        Karyawan2.setcal();
+        Karyawan3.setcal();
+        
+        
+        
         
         
 }
