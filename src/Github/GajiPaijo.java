da0ca9998e0021c03c2c3ea0f4758c2bcddbf727
dwiyan <dwiyan774@gmail.com>
2016-03-09 17:12:47 +0700

    mencari gaji paijo

--- src/Github/GajiPaijo.java
+++ src/Github/GajiPaijo.java
@@ -5,13 +5,34 @@
  */
 package Github;
 
+import java.text.ParseException;
+
 /**
  *
  * @author dwiyan
  */
 public class GajiPaijo {
-   public void jo(){
-    JadwalKaryawan m1 = new JadwalKaryawan();
-   
-   }
+
+    /* HitungGaji m2 = new HitungGaji();
+     //    JadwalKaryawan m1 = new JadwalKaryawan();
+
+     public double joDenda() throws ParseException {
+
+     return m2.hitungDenda("2016-01-15","08:00", "17:00", 75);
+
+     }
+     */
+    public static void main(String[] args) throws ParseException {
+        HitungGaji m2 = new HitungGaji();
+        double a = m2.hitungDenda("2016-01-11", "10:00", "17:00", 75);
+        System.out.println(a);
+
+        double b = m2.hitungBonus("2016-01-15", "08:00", "18:00", 150);
+        System.out.println(b);
+
+        double c = m2.hitungBonus("2016-01-15", "08:00", "18:00", 150);
+        System.out.println(c);
+        
+        
+    }
 }
