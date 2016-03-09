d5e37c0e2249a445dac44575846e8057c23cfc5c
Enggar Pratama Kurniawan <itsenggar@gmail.com>
2016-03-09 14:44:21 +0700

    penambahan import calender DLL

---
+++ src/pemrosesan_kalender.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ *
+ * @author Enggar Pratama
+ */
+public class pemrosesan_kalender {
+    private String tanggal;
+    private String bulan;
+    private String tahun;
+    private String hari;
+    
+}
