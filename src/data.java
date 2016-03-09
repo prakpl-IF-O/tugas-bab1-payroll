e7c2a619322953da4a302a8ab902010832f13fb9
Husein Abdulbar <abdulbarhusein@yahoo.com>
2016-03-09 16:14:13 +0700

    penambahan hari libur dan finishing program

--- src/data.java
+++ src/data.java
@@ -1,38 +1,56 @@
 import java.util.Calendar;
 public class data {
-    int tg, gaji, bon,bo,de, tun, den, t, b, h,hm,mm,hos,mis,i;
+    int tg, gaji, bon,bo,de, tun, den, t, b, h,hm,mm,hos,mis,d;
     String nik, nama;
+    boolean day=false;
     Calendar cal1 = Calendar.getInstance();
     Calendar cal2 = Calendar.getInstance();
     public void setGaji(int gaji, int tu, int bo) {
         this.gaji = gaji;
-        this.bo=bo;      
-        if(hos>17){
-            bon=bo*(hos-17);
-        }else if(i==cal1.FRIDAY){
-         bon=bo*(hos-hm);   
-        }else{
-        bon = 0;
+        this.bo=bo;
+        tun = tu;
+        hari(d);
+        if(day==true){
+         bon=bo*(hos-hm);
+         if(mis>29){
+            bon=bon+bo/2;
         }
+        }else if(hos>=17){
+            bon=bo*(hos-17);
+        
         if(mis>29){
             bon+=bo/2;
         }
-        tun = tu;
+        }
+    }
+    public void hari(int d){
+        if(d==2||d==3){day=true;}
+        else if((d-2)%7==0){
+        day=true;
+    }else if((d-3)%7==0){
+        day=true;
+    }          
     }
     public void setDen(int de) {
         this.de=de;
-        if(hos<17||hm>8){
+        if(hos<=17){
             den=de*(17-hos)+de*(hm-8);
+            if(mis>29){
+            den+=de/2;
+        }if(hm>=8){
+                if(mm>29){
+            den+=de/2;
+        }
         }else{
         den = 0;
         }
-        if(mis>29||mm>29){
-            den+=de/2;
-        }
+        
+    }    
     }
     public void calendar(int y,int m,int d, int hm, int mm,int hs,int ms){
         cal1.set(y, m, d, hm, mm,0);
         cal2.set(y,m,d,hs,ms,0);
+        this.d=d;
         hos=hs;this.hm=hm;
         mis=ms;this.mm=mm;
     }
