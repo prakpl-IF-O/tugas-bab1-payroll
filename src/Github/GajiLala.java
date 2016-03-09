4d841c5c3a77cb832a924cd48215d4747f8b6760
dwiyan <dwiyan774@gmail.com>
2016-03-09 18:02:15 +0700

    Membuat method untuk menghitung total gaji Lala

--- src/Github/GajiLala.java
+++ src/Github/GajiLala.java
@@ -12,24 +12,45 @@ import java.text.ParseException;
  * @author dwiyan
  */
 public class GajiLala {
-    public static void main(String[] args) throws ParseException {
-        
+
+    public double dendaLala() throws ParseException {
         HitungGaji m2 = new HitungGaji();
-        double denda = m2.hitungDenda("2016-01-15", "08:00", "17:00", 75);
+
+        double denda = m2.hitungDenda("2016-01-15", "08:00", "17:00", 125);
+        return denda;
+
+    }
+
+    public double bonusLala() throws ParseException {
+        HitungGaji m2 = new HitungGaji();
+
+        double bonus = m2.hitungDenda("2016-01-15", "08:00", "17:00", 200);
+        return bonus;
+    }
+
+    public double totalPaijo() throws ParseException {
+        double a = bonusLala();
+        double b = dendaLala();
+
+        double totalGaji = 900 + 200 + a - b;
+
+        return totalGaji;
+    }
+
+    public static void main(String[] args) throws ParseException {
+
+        HitungGaji m2 = new HitungGaji();
+        double denda = m2.hitungDenda("2016-01-15", "08:00", "17:00", 125);
         System.out.println(denda);
 
-        double bonus2 = m2.hitungBonus("2016-01-15", "08:00", "17:00", 150);
+        double bonus2 = m2.hitungBonus("2016-01-15", "08:00", "17:00", 200);
         System.out.println(bonus2);
 
-        double bonus1 = m2.hitungBonus("2016-01-16", "08:00", "18:30", 150);
-
-        System.out.println(bonus1);
-        double gajiPaijo = 900 + 200 + bonus1 + bonus2 - denda;
+        double gajiPaijo = 1000 + 350 + bonus2 - denda;
         System.out.println(gajiPaijo);
     }
-    public void lalaGaji(){
-        
-     
-       
+
+    public void lalaGaji() {
+
     }
 }
