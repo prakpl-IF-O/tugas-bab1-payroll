24baae331f16506b1acbfc9f6c4cd50672ecad9d
suastikaadinata <suastikaadinata97@gmail.com>
2016-03-10 00:45:11 +0700

    Program selesai

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -8,11 +7,6 @@ import java.util.Calendar;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-//nik,tanggal,jam_masuk,jam_pulang // column header, tidak disimpan
-//101,2016-01-15,08:00,17:00
-//101,2016-01-16,08:00,18:30
-//102,2016-01-15,08:00,15:00
-//103,2016-01-15,08:00,17:00
 
 /**
  *
@@ -29,8 +23,6 @@ public class Main {
         double[] total_gaji = new double[4];
         Karyawan k[] = new Karyawan[4];
         Absensi a[] = new Absensi[4];
-        String hari1 = "Saturday";
-        String hari2 = "Sunday";
 
 //        nik,nama,gaji,tunjangan,bonus,denda
         k[0] = new Karyawan(101, "paijo", 900, 200, 150, 75);
@@ -47,41 +39,81 @@ public class Main {
         Calendar calendar = Calendar.getInstance();
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd");
         SimpleDateFormat dateFormat1 = new SimpleDateFormat("EEEE");
-
-//        System.out.println(calendar.get(Calendar.DAY_OF_WEEK));
-//        String formatted = dateFormat.format(tanggal);
         for (int i = 0; i < 4; i++) {
-            if (masuk < a[i].jam_masuk) {
+
+            if (masuk < Math.floor(a[i].jam_masuk)) {
                 denda[i] = 0;
             } else {
-                denda[i] = k[i].denda * (a[i].jam_masuk - masuk);
+                denda[i] = k[i].denda * (Math.floor(a[i].jam_masuk) - masuk);
             }
 
-            if (pulang > a[i].jam_pulang) {
+            if (pulang > Math.floor(a[i].jam_pulang)) {
                 bonus[i] = 0;
-                denda1[i] = (pulang - a[i].jam_pulang) * k[i].denda;
+                denda1[i] = (pulang - Math.floor(a[i].jam_pulang)) * k[i].denda;
             } else {
-                bonus[i] = (a[i].jam_pulang - pulang) * k[i].bonus;
+                bonus[i] = (Math.floor(a[i].jam_pulang) - pulang) * k[i].bonus;
                 denda1[i] = 0;
             }
+
             Date tanggal = dateFormat.parse(a[i].tanggal);
             calendar.setTime(tanggal);
             String tanggal1 = dateFormat1.format(calendar.getTime());
             denda[i] += denda1[i];
-            System.out.println(a[i].tanggal);
-            System.out.println(tanggal1);
-            String z = tanggal1;
-            if ((z.equals(hari1)) || (z.equals(hari2))) {
-                bonus[i] = (a[i].jam_pulang - a[i].jam_masuk) * k[i].bonus;
-                System.out.println(bonus[i]);
+
+            if ((tanggal1.equals("Saturday")) || (tanggal1.equals("Sunday"))) {
+                bonus[i] = (Math.floor(a[i].jam_pulang)
+                        - Math.floor(a[i].jam_masuk)) * k[i].bonus;
             }
-            System.out.println("z: " + z);
-            System.out.println("Bonus: " + bonus[i] + " Denda: " + denda[i]);
         }
+        
+        System.out.println("DATA KARYAWAN");
+        System.out.println("NIK       NAMA      GAJI      TUNJANGAN BONUS     "
+                + "DENDA");
+        for (int i = 1;i<4;i++){
+            System.out.printf("%-10s", k[i].nik);
+            System.out.printf("%-10s", k[i].nama);
+            System.out.printf("%-10s", k[i].gaji);
+            System.out.printf("%-10s", k[i].tunjangan);
+            System.out.printf("%-10s", k[i].bonus);
+            System.out.printf("%-10s", k[i].denda);
+            System.out.println("");
+        }
+
         System.out.println("");
-        for (int i = 0; i < 4; i++) {
-            total_gaji[i] = (k[i].gaji + k[i].tunjangan + bonus[i]) - denda[i];
-            System.out.println(total_gaji[i]);
+        System.out.println("DATA ABSENSI");
+        System.out.println("NIK         TANGGAL     JAM MASUK   JAM PULANG");       
+        for (int i = 0;i<4;i++){
+            System.out.printf("%-12s", a[i].nik);
+            System.out.printf("%-12s", a[i].tanggal);
+            System.out.printf("%-12s","0"+a[i].jam_masuk+"0");
+            System.out.printf("%-12s",+a[i].jam_pulang+"0");
+            System.out.println("");
+        }
+        
+        System.out.println("");
+        System.out.println("===================================================");
+        System.out.println("");
+        System.out.println("DATA HASIL PENGOLAHAN TOTAL GAJI");
+        System.out.println("NIK       NAMA      GAJI      TUNJANGAN BONUS     "
+                + "DENDA     TOTAL GAJI");
+        for (int i = 1; i < 4; i++) {
+            for (int j = 1; j < 4; j++) {
+                if (k[i].nik == a[i].nik) {
+                    total_gaji[i] = (k[i].gaji + k[i].tunjangan + (bonus[i]
+                            + bonus[j])) - (denda[i] + denda[j]);
+                } else {
+                    total_gaji[i] = (k[i].gaji + k[i].tunjangan + bonus[i]) 
+                            - denda[i];
+                }
+            }
+            System.out.printf("%-10s", k[i].nik);
+            System.out.printf("%-10s", k[i].nama);
+            System.out.printf("%-10s", k[i].gaji);
+            System.out.printf("%-10s", k[i].tunjangan);
+            System.out.printf("%-10s", k[i].bonus);
+            System.out.printf("%-10s", k[i].denda);
+            System.out.printf("%-10s", total_gaji[i]);
+            System.out.println("");
         }
     }
 }
