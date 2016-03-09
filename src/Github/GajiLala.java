050e85c4bb2a1f13d1acd26d3dbef967336b01ae
dwiyan <dwiyan774@gmail.com>
2016-03-09 17:54:01 +0700

    Mengecek proses penghitungan dari kelas GajiLala

--- src/Github/GajiLala.java
+++ src/Github/GajiLala.java
@@ -5,10 +5,31 @@
  */
 package Github;
 
+import java.text.ParseException;
+
 /**
  *
  * @author dwiyan
  */
 public class GajiLala {
-    
+    public static void main(String[] args) throws ParseException {
+        
+        HitungGaji m2 = new HitungGaji();
+        double denda = m2.hitungDenda("2016-01-15", "08:00", "17:00", 75);
+        System.out.println(denda);
+
+        double bonus2 = m2.hitungBonus("2016-01-15", "08:00", "17:00", 150);
+        System.out.println(bonus2);
+
+        double bonus1 = m2.hitungBonus("2016-01-16", "08:00", "18:30", 150);
+
+        System.out.println(bonus1);
+        double gajiPaijo = 900 + 200 + bonus1 + bonus2 - denda;
+        System.out.println(gajiPaijo);
+    }
+    public void lalaGaji(){
+        
+     
+       
+    }
 }
