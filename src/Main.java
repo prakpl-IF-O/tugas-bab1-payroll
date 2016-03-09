803e66742a3cb4e071c12251185a0ce4639aa203
suastikaadinata <suastikaadinata97@gmail.com>
2016-03-09 19:02:26 +0700

    Mulai membuat codingan untuk menentukan hari berdasarkan inputan tanggal

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,8 @@
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import 
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -48,6 +53,10 @@ public class Main {
                 bonus[i]=(a[i].jam_pulang-pulang)*k[i].bonus;
                 denda[i]=0;
             }
+            
+           SimpleDateFormat format = new SimpleDateFormat("yyyy-mm-dd");
+           Date tanggal = format.parse("2016-01-15");
+           Calender calender = Calender.getInstance();
         }
 
     }
