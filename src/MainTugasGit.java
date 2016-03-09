2f087d372aa84630226dd83dc64898ddec3ee4f2
mfnaim <naimfattahp@gmail.com>
2016-03-09 20:22:35 +0700

    Create MainTugasGit.java
    membuat main class

---
+++ src/MainTugasGit.java
@@ -0,0 +1,11 @@
+package maintugasgit;
+public class MainTugasGit {
+   public static void main(String[] args) {
+   TugasGit oyi = new TugasGit();
+   oyi.DataKaryawan();
+   System.out.println();
+   oyi.Absensi();
+   System.out.println();
+   oyi.Hasil();
+  }
+}
