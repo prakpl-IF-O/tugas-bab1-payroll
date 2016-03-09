5948608d4a9143166d116f426955c22b7fb929d0
andinadea <anidnadhea97@gmail.com>
2016-03-09 19:43:11 +0700

    nyoba-nyoba dulu bikin kelasnya

---
+++ src/OOP/Git.java
@@ -0,0 +1,47 @@
+package OOP;
+
+import java.util.Scanner;
+
+public class Git {
+        Scanner in = new Scanner (System.in);
+        public String Nama, NIK;
+        public double Gaji, Total, Tunjangan, Bonus, Denda, jam_masuk, jam_pulang, totalGaji;
+        public String Tgl;
+             
+        public void setNIK(String a){
+            NIK = a;
+        }
+        public void setNama(String b){
+            Nama = b;
+        }
+        public void setGaji(Double c){
+            Gaji = c;
+        }
+        public void setTunjangan(Double d){
+            Tunjangan = d;
+        }
+        public void setBonus(Double e){
+            Bonus = e;
+        }
+        public void setDenda(Double f){
+            Denda = f;
+        }
+        public void setTgl(String g){
+            Tgl = g;
+        }
+        public void setjam_masuk(Double h){
+            jam_masuk = h;
+        }
+        public void setjam_pulang(Double i){
+            jam_pulang = i;
+        }
+        public void settotalGaji(Double j){
+            totalGaji = j;
+        }
+        public void display(){
+            
+                
+            }      
+        }
+
+
