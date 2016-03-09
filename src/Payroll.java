98786270c93683d393188e5ff01d9cd909ff9479
Dwiki Prayogo Wicaksono <dwikiprayogo97@gmail.com>
2016-03-09 09:43:58 +0700

    Rename 2 class

--- src/Payroll.java
+++ src/Payroll.java
@@ -1,6 +1,4 @@
 
 public class Payroll {
-    public static void main(String[] args) {
-        
-    }
+    
 }
---
+++ src/mainPayroll.java
@@ -0,0 +1,6 @@
+
+public class mainPayroll {
+    public static void main(String[] args) {
+        
+    }
+}
