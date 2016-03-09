9462d6d451619d5b12cc3a50ca3aff97fad2c328
Dwiki Prayogo Wicaksono <dwikiprayogo97@gmail.com>
2016-03-09 11:27:10 +0700

    Penambahan beberapa variabel beserta method setter dan getter (class Payroll)

--- src/Payroll.java
+++ src/Payroll.java
@@ -1,4 +1,44 @@
-
+import java.util.Calendar;
 public class Payroll {
+    Calendar kal=Calendar.getInstance();
+    private String nama;
+    private int nik, gaji, tunjangan, bonus, denda, totalgaji, jam, menit,hari;
+    
+    public void setHari(int thn,int bln,int hr){
+        kal.set(thn, bln, hr);
+        hari = kal.get(Calendar.DAY_OF_WEEK);
+    }
+    
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public void setNik(int nik) {
+        this.nik = nik;
+    }
+
+    public void setGaji(int gaji) {
+        this.gaji = gaji;
+    }
+
+    public void setTunjangan(int tunjangan) {
+        this.tunjangan = tunjangan;
+    }
+
+    public void setJam(int jam) {
+        this.jam = jam;
+    }
+
+    public void setMenit(int menit) {
+        this.menit = menit;
+    }
+    
+    public void setBonusDenda(int jam,int menit,String hari){
+        
+    }
+    
+    public void setTotalgaji() {
+        totalgaji = (gaji + tunjangan + bonus) - denda;
+    }
     
 }
