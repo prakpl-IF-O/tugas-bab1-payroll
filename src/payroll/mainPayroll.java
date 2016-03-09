c6ffbd49d374010cc82fa99f6c4a701b61496a01
Faza Abdi Perkasa <abdifaza@yahoo.com>
2016-03-09 16:58:28 +0700

    Penambahan arrayList

--- src/payroll/mainPayroll.java
+++ src/payroll/mainPayroll.java
@@ -5,9 +5,7 @@
  */
 package payroll;
 
-import java.text.DateFormat;
-import java.text.Format;
-import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 
 /**
  *
@@ -16,9 +14,16 @@ import java.text.SimpleDateFormat;
 public class mainPayroll {
 
     public static void main(String[] args) throws Exception {
-        DateFormat formatter1;
-        formatter1 = new SimpleDateFormat("yyyy-MM-dd");
-        Format formatter = new SimpleDateFormat("EEE");
-        String today = formatter.format(formatter1.parse("2016-01-16"));
+        ArrayList<dataKaryawan> dk = new ArrayList();
+        dataKaryawan dk1 = new dataKaryawan(101, "paijo", 900, 200, 150, 75);
+        dataKaryawan dk2 = new dataKaryawan(102, "lulu", 875, 200, 100, 100);
+        dataKaryawan dk3 = new dataKaryawan(103, "lala", 1000, 350, 200, 125);
+
+        ArrayList<dataAbsensi> da = new ArrayList();
+        dataAbsensi da1 = new dataAbsensi(101,"2016-01-15",08.00,17.00);
+        dataAbsensi da2 = new dataAbsensi(101,"2016-01-16",08.00,18.30);
+        dataAbsensi da3 = new dataAbsensi(102,"2016-01-15",08.00,15.00);
+        dataAbsensi da4 = new dataAbsensi(103,"2016-01-15",08.00,17.00);
+        
     }
 }
