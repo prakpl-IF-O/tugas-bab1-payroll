2ca7a3b17baa087b2eb40542ab53d46e8a4a8960
Satria Adhi Kharisma <SAKharisma@gmail.com>
2016-03-09 20:43:29 +0700

    Penambahan serta pengeditan Bonus dan Denda Karyawan

--- src/Karyawan.java
+++ src/Karyawan.java
@@ -14,13 +14,31 @@ public class Karyawan {
     public int Total_Gaji;
     public int Jam_masuk;
     public int Jam_pulang;
+    int jm1;
+    int mm1;
+    int jk1;
+    int mk1;
+    int jm;
+    int mm;
+    int jk;
+    int mk;
 
-    public void setTanggalMasuk(){
-        Tanggal1.set(2016, 0, 15, 8, 00);
+    public void setTanggalK(int t, int b, int h, int jm1, int mm1, int jk1, int mk1) {
+        Tanggal.set(t, b, h, jm1, mm1);
+        Tanggal1.set(t, b, h, jk1, mk1);
+        this.jm1 = jm1;
+        this.mm1 = mm1;
+        this.jk1 = jk1;
+        this.mk1 = mk1;
     }
-    public void setTanggal1(int t, int b, int h, int j, int m) {
-        Tanggal.set(t, b, h, j, m);
 
+    public void setTanggal(int t, int b, int h, int jm, int mm, int jk, int mk) {
+        Tanggal.set(t, b, h, jm, mm);
+        Tanggal1.set(t, b, h, jk, mk);
+        this.jm = jm;
+        this.mm = mm;
+        this.jk = jk;
+        this.mk = mk;
     }
 
     public void setNama(String n) {
@@ -41,21 +59,44 @@ public class Karyawan {
 
     public void setBonus(int b) {
         Bonus = b;
+
+        if (jk1 > jk) {
+            Bonus = Bonus * (jk1 - jk);
+            if (mk1 > mk) {
+                Bonus = Bonus * (mk1 - mk);
+            }
+        }
+
     }
 
     public void setDenda(int d) {
         Denda = d;
+
+        if (jm1 > jm) {
+            System.out.println(Denda);
+            Denda = Denda * (jm1 - jm);
+
+            if (mm1 > mm) {
+                Denda = Denda * (mm1 - mm);
+            }
+        }
     }
 
     public void setTotalgaji() {
         Total_Gaji = (Gaji + Tunjangan + Bonus) - Denda;
     }
+
     public void displayMessage() {
         System.out.println("Nik,Nama,Gaji,Tunjangan,Bonus,Denda,Total Gaji");
         System.out.println(Nik + "," + Nama + "," + Gaji + "," + Tunjangan + "," + Bonus + "," + Denda + "," + Total_Gaji);
-        
-        System.out.println(Tanggal1.getTime());
+        System.out.println("Tanggal, Jam Masuk");
         System.out.println(Tanggal.getTime());
+        System.out.println("Tanggal, Jam Keluar");
+        System.out.println(Tanggal1.getTime());
+    }
+
+    public void Waktu() {
+
     }
 
 }
--- src/MainPayroll.java
+++ src/MainPayroll.java
@@ -1,3 +1,4 @@
+
 public class MainPayroll {
 
     public static void main(String[] args) {
@@ -7,18 +8,15 @@ public class MainPayroll {
         k1.setNama("Satria");
         k1.setGaji(900);
         k1.setTunjangan(200);
-        k1.setBonus(150);
-        k1.setDenda(75);
-        k1.setTotalgaji();
-        k1.setTanggalMasuk();
-        k1.setTanggal1(2016, 2, 9, 14, 00);
-    
-        k1.displayMessage();
         
 
-       
-        
-        
+        k1.setTanggalK(2016, 2, 9, 10, 0, 19, 00);
+        k1.setTanggal(2016, 2, 9, 8, 0, 17, 00);
+        k1.setDenda(75);
+        k1.setBonus(150);
+        k1.setTotalgaji();
+        k1.displayMessage();
+
     }
 
 }
