40b79c60c2cc2a8852ac82c58abba620c25e6555
Enggar Pratama Kurniawan <itsenggar@gmail.com>
2016-03-09 16:39:48 +0700

    menginputkan data karyawan

--- src/MAINCLASS.java
+++ src/MAINCLASS.java
@@ -13,6 +13,10 @@ public class MAINCLASS {
     pengolahan_gaji_krywn kry1 = new pengolahan_gaji_krywn (101, "paijo", 900, 150, 125 , 200);
     pengolahan_gaji_krywn kry2 = new pengolahan_gaji_krywn (101, "paijo", 900, 150, 125 , 200);
     pengolahan_gaji_krywn kry3 = new pengolahan_gaji_krywn (101, "paijo", 900, 150, 125 , 200);
-    
+
+    kry1.check_in(0, 8, 2016, 01, 15,  17, 101);
+    kry1.check_in(1, 8, 2016, 01, 16,  18, 101);
+    kry2.check_in(2, 8, 2016, 01, 15,  15, 102);
+    kry3.check_in(3, 8, 2016, 01, 15,  17, 103);
     }
 }
--- src/pengolahan_gaji_krywn.java
+++ src/pengolahan_gaji_krywn.java
@@ -16,7 +16,12 @@ public class pengolahan_gaji_krywn {
     private int denda;    
     private int hasil_gaji;
     private int tunjangan;
-    
+    private int jam_mas[] = new int [5000];
+    private int year[] = new int [5000];
+    private int month [] = new int [5000];
+    private int day [] = new int [5000];
+    private int jam_kel [] = new int [5000];
+    private int NIK_absensi[] = new int [5000];
     public pengolahan_gaji_krywn(int NIK1, String nama1, int gaji1, int bonus1 , int denda1, int tunjangan1){
        NIK = NIK1;
        nama = nama1;
@@ -24,6 +29,17 @@ public class pengolahan_gaji_krywn {
        bonus = bonus1;
        denda = denda1;
        tunjangan = tunjangan1;
+    }
+    public void check_in (int urutan, int jam_mas1, int year1, int month1, int day1, int jam_kel1, int NIK_absensi1){
+       jam_mas[urutan] = jam_mas1;
+       year[urutan] = year1;
+       month[urutan] = month1;
+       day[urutan] = day1;
+       jam_kel[urutan] = jam_kel1;
+       NIK_absensi[urutan] = NIK_absensi1;
+    }
+    public int salary (int NIK){
+        
     }
     
 }
