677e392822e33689ff0e8c4bc050a5f6f9b1f207
M Khairullahh Harto <m.k.harto96@gmail.com>
2016-03-09 13:55:25 +0700

    kelas karyawan dengan atribut nama(string), nik, gaji, tunjangan, bonus, denda(short)

---
+++ src/tugasPraktikum/Karyawan.java
@@ -0,0 +1,17 @@
+package tugasPraktikum;
+public class Karyawan {
+    private String nama;
+    private short nik;
+    private short gaji;
+    private short denda;
+    private short tunjangan;
+    private short bonus;
+    
+    public void setNama(String a){
+        nama=a;
+    }
+    public void setNik(short a){
+        nik=a;
+    }
+    
+}
