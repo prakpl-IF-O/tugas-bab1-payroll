e6ef1b08e0b74d61d2e1e5985fe1f113aae73e5a
suastikaadinata <suastikaadinata97@gmail.com>
2016-03-09 15:48:27 +0700

    Deklarasi variabel dan membuat method yang diperlukan di class Absensi

--- src/Absensi.java
+++ src/Absensi.java
@@ -3,11 +3,23 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+//nik,tanggal,jam_masuk,jam_pulang // column header, tidak disimpan
+//101,2016-01-15,08:00,17:00
+//101,2016-01-16,08:00,18:30
+//102,2016-01-15,08:00,15:00
+//103,2016-01-15,08:00,17:00
 /**
  *
  * @author gunka
  */
 public class Absensi {
-    
+   int nik;
+   String tanggal, jam_masuk, jam_pulang;
+   
+   Absensi(int nik, String tanggal, String jam_masuk, String jam_pulang){
+       this.nik = nik;
+       this.tanggal = tanggal;
+       this.jam_masuk = jam_pulang;
+       this.jam_pulang = jam_pulang;
+   }
 }
