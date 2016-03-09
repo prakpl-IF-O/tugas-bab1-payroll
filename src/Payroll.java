5dce5ef01a125282422316ae7567426c3b4f1d83
Dwiki Prayogo Wicaksono <dwikiprayogo97@gmail.com>
2016-03-09 16:18:44 +0700

    Penambahan fungsi - fungsi pengolah data karyawan

--- src/Payroll.java
+++ src/Payroll.java
@@ -4,11 +4,14 @@ import java.util.Calendar;
 public class Payroll {
 
     Calendar kal = Calendar.getInstance();
-    private String nama;
-    private int nik, gaji, tunjangan, bonus, denda, totalgaji, jam, menit, hari;
+    private String nama, hariString;
+    private int nik, gaji, tunjangan, bonus, denda, totalgaji, jamMasuk, jamPulang, menitMasuk, menitPulang, hari, tanggal, tahun, bulan;
 
-    public void setHari(int thn, int bln, int hr) {
-        kal.set(thn, bln-1, hr);
+    public void setHari(int thn, int bln, int tgl) {
+        tahun = thn;
+        bulan = bln;
+        tanggal = tgl;
+        kal.set(thn, bln - 1, tgl - 1);
         hari = kal.get(Calendar.DAY_OF_WEEK);
     }
 
@@ -28,20 +31,79 @@ public class Payroll {
         this.tunjangan = tunjangan;
     }
 
-    public void setJam(int jam) {
-        this.jam = jam;
+    public void setJamMasuk(int jamMasuk) {
+        this.jamMasuk = jamMasuk;
     }
 
-    public void setMenit(int menit) {
-        this.menit = menit;
+    public void setJamPulang(int jamPulang) {
+        this.jamPulang = jamPulang;
     }
 
-    public void setBonusDenda(int jam, int menit, String hari) {
-        
+    public void setMenitMasuk(int menitMasuk) {
+        this.menitMasuk = menitMasuk;
+    }
+
+    public void setMenitPulang(int menitPulang) {
+        this.menitPulang = menitPulang;
+    }
+
+    public void setBonus() {
+        if (hari == 6 || hari == 7) {
+            if (menitPulang >= menitMasuk) {
+                bonus += (jamPulang - jamMasuk) * 100;
+            } else {
+                bonus += ((jamPulang - jamMasuk) - 1) * 100;
+            }
+        } else if (jamMasuk <= 8) {
+            if (jamPulang >= 17) {
+                bonus += (jamPulang - 17) * 50;
+            } else {
+                denda += 50;
+            }
+        } else {
+            denda += (jamMasuk - 8) * 50;
+            if (jamPulang >= 17) {
+                bonus += (jamPulang - 17) * 50;
+            } else {
+                denda += 50;
+            }
+        }
     }
 
     public void setTotalgaji() {
-        totalgaji = (gaji + tunjangan + bonus) - denda;
+        totalgaji += (gaji + tunjangan + bonus) - denda;
     }
 
+    public String hariString() {
+        switch (hari) {
+            case 1:
+                return hariString = "Senin";
+            case 2:
+                return hariString = "Selasa";
+            case 3:
+                return hariString = "Rabu";
+            case 4:
+                return hariString = "Kamis";
+            case 5:
+                return hariString = "Jum'at";
+            case 6:
+                return hariString = "Sabtu";
+            case 7:
+                return hariString = "Minggu";
+            default:
+                return "null";
+        }
+    }
+
+    public void display() {
+        System.out.println("Nama : " + nama);
+        System.out.println("NIK : " + nik);
+        System.out.println("Gaji : " + gaji);
+        System.out.println("Tunjangan : " + tunjangan);
+        System.out.printf("Hari : %s ,%d-%d-%d\n", hariString(), tanggal, bulan, tahun);
+        System.out.printf("Jam : %02d:%02d s/d %02d:%02d\n", jamMasuk, menitMasuk, jamPulang, menitPulang);
+        System.out.println("Bonus : " + bonus);
+        System.out.println("Denda : " + denda);
+        System.out.println("Total Gaji : " + totalgaji);
+    }
 }
--- src/mainPayroll.java
+++ src/mainPayroll.java
@@ -1,12 +1,14 @@
+
 public class mainPayroll {
+
     public static void main(String[] args) {
         String nama;
-        int nik, gaji, tunjangan, jam, menit, thn,bln,hr;
-        
+        int nik, gaji, tunjangan, jamMasuk, jamPulang, menitMasuk, menitPulang, thn, bln, tgl;
+
         Payroll karyawan0 = new Payroll();
         Payroll karyawan1 = new Payroll();
         Payroll karyawan2 = new Payroll();
-        
+
         //Data karyawan
         nama = "Aki";
         karyawan0.setNama(nama);
@@ -16,7 +18,7 @@ public class mainPayroll {
         karyawan0.setGaji(gaji);
         tunjangan = 300;
         karyawan0.setTunjangan(tunjangan);
-        
+
         nama = "Boni";
         karyawan1.setNama(nama);
         nik = 102;
@@ -25,7 +27,7 @@ public class mainPayroll {
         karyawan1.setGaji(gaji);
         tunjangan = 300;
         karyawan1.setTunjangan(tunjangan);
-        
+
         nama = "Cecep";
         karyawan2.setNama(nama);
         nik = 103;
@@ -34,6 +36,38 @@ public class mainPayroll {
         karyawan2.setGaji(gaji);
         tunjangan = 300;
         karyawan2.setTunjangan(tunjangan);
-        
+
+        //Absensi Karyawan
+        tgl = 9;
+        bln = 3;
+        thn = 2016;
+        karyawan0.setHari(thn, bln, tgl);
+        jamMasuk = 8;
+        karyawan0.setJamMasuk(jamMasuk);
+        menitMasuk = 00;
+        karyawan0.setMenitMasuk(menitMasuk);
+        jamPulang = 16;
+        karyawan0.setJamPulang(jamPulang);
+        menitPulang = 59;
+        karyawan0.setMenitPulang(menitPulang);
+        karyawan0.setBonus();
+        karyawan0.setTotalgaji();
+        karyawan0.display();
+        tgl = 12;
+        bln = 3;
+        thn = 2016;
+        karyawan0.setHari(thn, bln, tgl);
+        jamMasuk = 8;
+        karyawan0.setJamMasuk(jamMasuk);
+        menitMasuk = 00;
+        karyawan0.setMenitMasuk(menitMasuk);
+        jamPulang = 9;
+        karyawan0.setJamPulang(jamPulang);
+        menitPulang = 00;
+        karyawan0.setMenitPulang(menitPulang);
+        karyawan0.setBonus();
+        karyawan0.setTotalgaji();
+        karyawan0.display();
+
     }
 }
