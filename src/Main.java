1a5f12dcc9eb4c99408c84917ef726e714c6c087
suastikaadinata <suastikaadinata97@gmail.com>
2016-03-09 17:28:54 +0700

    Selesai membuat rumus untuk menghitung denda dan bonus berdasarkan jam masuk dan jam pulang

--- src/Main.java
+++ src/Main.java
@@ -8,36 +8,48 @@
 //101,2016-01-16,08:00,18:30
 //102,2016-01-15,08:00,15:00
 //103,2016-01-15,08:00,17:00
+
 /**
  *
  * @author gunka
  */
 public class Main {
-    
+
     public static void main(String[] args) {
         double masuk = 08.00;
         double pulang = 17.00;
-        double []bonus = new double[3];
-        double []denda = new double[3];
+        double[] bonus = new double[3];
+        double[] denda = new double[3];
         Karyawan k[] = new Karyawan[3];
         Absensi a[] = new Absensi[4];
 //        nik,nama,gaji,tunjangan,bonus,denda
-        k[0] = new Karyawan(101,"paijo",900,200,150,75);
-        k[1] = new Karyawan(102,"lulu",875,200,100,100);
-        k[2] = new Karyawan(103,"lala",1000,350,200,125);
+        k[0] = new Karyawan(101, "paijo", 900, 200, 150, 75);
+        k[1] = new Karyawan(102, "lulu", 875, 200, 100, 100);
+        k[2] = new Karyawan(103, "lala", 1000, 350, 200, 125);
         //nik,tanggal,jam_masuk,jam_pulang
-        a[0] = new Absensi(101,"2016-01-15",08.00,17.00);
-        a[1] = new Absensi(101,"2016-01-16",08.00,18.30);
-        a[2] = new Absensi(102,"2016-01-15",08.00,15.00);
-        a[3] = new Absensi(102,"2016-01-15",08.00,17.00);
-               
-//        if (masuk>a[1].jam_masuk){
-//            denda = k[1].denda*(a[1].jam_masuk-masuk);
-//        }
-        
-        
-        
-        
+        a[0] = new Absensi(101, "2016-01-15", 08.00, 17.00);
+        a[1] = new Absensi(101, "2016-01-16", 08.00, 18.30);
+        a[2] = new Absensi(102, "2016-01-15", 08.00, 15.00);
+        a[3] = new Absensi(102, "2016-01-15", 08.00, 17.00);
+
+        for (int i = 0; i < 3; i++) {
+            if (masuk < a[i].jam_masuk) {
+                denda[i] = k[i].denda * (a[i].jam_masuk - masuk);                
+            }
+            else {
+                denda[i]=0;
+            }
+            
+            if (pulang>a[i].jam_pulang){
+                bonus[i]=0;
+                denda[i]=(pulang-a[i].jam_pulang)*k[i].denda;
+            }
+            else {
+                bonus[i]=(a[i].jam_pulang-pulang)*k[i].bonus;
+                denda[i]=0;
+            }
+        }
+
     }
-    
+
 }
