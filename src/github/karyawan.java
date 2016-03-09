adc71848933426e483e19892bfffe0edbed212c3
Khrisna WIdhi <khrisnaww@gmail.com>
2016-03-09 21:00:17 +0700

    buat class absensi

--- src/github/karyawan.java
+++ src/github/karyawan.java
@@ -1,14 +1,43 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package github;
-
-/**
- *
- * @author Khrisna
- */
 public class karyawan {
+    private String nama;
+    private String nik;
+    private int gaji;
+    private int tunjangan;
+    private int bonus;
+    private int denda;
+
+    public void karyawan(String Nama,String NIK,int Gaji, int Tunjangan, int Bonus, int Denda){
+                nama=Nama;
+                nik=NIK;
+                gaji=Gaji;
+                tunjangan=Tunjangan;
+                bonus=Bonus;
+                denda=Denda;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public String getNik() {
+        return nik;
+    }
+
+    public int getGaji() {
+        return gaji;
+    }
+
+    public int getTunjangan() {
+        return tunjangan;
+    }
+
+    public int getBonus() {
+        return bonus;
+    }
+
+    public int getDenda() {
+        return denda;
+    }
     
 }
--- src/github/maingaji.java
+++ src/github/maingaji.java
@@ -1,14 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package github;
-
-/**
- *
- * @author Khrisna
- */
 public class maingaji {
+    public static void main(String[] args) {
+    karyawan x1=new karyawan();
+  x1.karyawan("Paijo", "101", 900, 200, 150, 75);
+    karyawan x2=new karyawan();
+  x2.karyawan("Lulu", "102", 875, 200, 100, 100);
+    karyawan x3=new karyawan();
+  x3.karyawan("Lala", "103", 1000, 350, 200, 125);  
+    absensi y1=new absensi();
+  y1.absensi("101", "2016-01-15", 08.00, 17.00);
+    absensi y2=new absensi();
+  y2.absensi("101", "2016-01-16", 08.00, 18.30);
+    absensi y3=new absensi();
+  y3.absensi("102", "2016-01-15", 08.00, 15.00);
+    absensi y4=new absensi();  
+  y4.absensi("103", "2016-01-15", 08.00, 17.00);
+    }
     
 }
