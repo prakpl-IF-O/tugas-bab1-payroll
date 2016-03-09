eec59702f0c7153da96f7e65174bf5b738f73794
Satria Adhi Kharisma <SAKharisma@gmail.com>
2016-03-09 16:11:57 +0700

    Penambahan Tanggal dan waktu dengan fungsi calendar dan beberapa editan lainnya

--- src/Karyawan.java
+++ src/Karyawan.java
@@ -1,5 +1,10 @@
+
+import java.util.Calendar;
+
 public class Karyawan {
 
+    Calendar Tanggal = Calendar.getInstance();
+    Calendar Tanggal1 = Calendar.getInstance();
     public String Nik;
     public String Nama;
     public int Gaji;
@@ -7,9 +12,16 @@ public class Karyawan {
     public int Bonus;
     public int Denda;
     public int Total_Gaji;
-    public String Tanggal;
     public int Jam_masuk;
     public int Jam_pulang;
+
+    public void setTanggalMasuk(){
+        Tanggal1.set(2016, 0, 15, 8, 00);
+    }
+    public void setTanggal1(int t, int b, int h, int j, int m) {
+        Tanggal.set(t, b, h, j, m);
+
+    }
 
     public void setNama(String n) {
         Nama = n;
@@ -23,25 +35,27 @@ public class Karyawan {
         Gaji = g;
     }
 
-    public void setTunjangan (int t){
+    public void setTunjangan(int t) {
         Tunjangan = t;
     }
-    
-    public void setBonus(int b){
+
+    public void setBonus(int b) {
         Bonus = b;
     }
-    
-    public void setDenda(int d){
+
+    public void setDenda(int d) {
         Denda = d;
     }
-    
-    public void setTotalgaji(){
+
+    public void setTotalgaji() {
         Total_Gaji = (Gaji + Tunjangan + Bonus) - Denda;
     }
-    
-    public void displayMessage(){
-        System.out.println("Nik | Nama | Gaji | Tunjangan | Bonus | Denda | Total Gaji");
-        System.out.println(Nik + " | "+ Nama + " | " + Gaji + " | " + Tunjangan + " | " + Bonus + " | " + Denda + " | " + Total_Gaji);
+    public void displayMessage() {
+        System.out.println("Nik,Nama,Gaji,Tunjangan,Bonus,Denda,Total Gaji");
+        System.out.println(Nik + "," + Nama + "," + Gaji + "," + Tunjangan + "," + Bonus + "," + Denda + "," + Total_Gaji);
+        
+        System.out.println(Tanggal1.getTime());
+        System.out.println(Tanggal.getTime());
     }
-    
+
 }
--- src/MainPayroll.java
+++ src/MainPayroll.java
@@ -1,21 +1,22 @@
-import java.util.Date;
 public class MainPayroll {
 
     public static void main(String[] args) {
 
-        Date tanggal = new Date();
-        
         Karyawan k1 = new Karyawan();
-        k1.setNIK("101");
-        k1.setNama("Paijo");
+        k1.setNIK("151");
+        k1.setNama("Satria");
         k1.setGaji(900);
         k1.setTunjangan(200);
         k1.setBonus(150);
         k1.setDenda(75);
         k1.setTotalgaji();
+        k1.setTanggalMasuk();
+        k1.setTanggal1(2016, 2, 9, 14, 00);
+    
         k1.displayMessage();
         
-        System.out.println(tanggal);        
+
+       
         
         
     }
