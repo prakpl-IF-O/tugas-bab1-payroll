5ed66af60657d1dc41f8c22911a2afd5df68b471
Yan Aditiya Nugraha <yanaditiya29@gmail.com>
2016-03-09 18:06:54 +0700

    Main Method fix

--- src/tugas1/MainKaryawan.java
+++ src/tugas1/MainKaryawan.java
@@ -16,14 +16,19 @@ public class MainKaryawan {
         k1.jamKeluar[0] = (17);
         k1.dendaPegawai1(k1.jamMasuk[0], k1.jamKeluar[0], k1.getDenda());
         k1.bonusPegawai1(k1.jamKeluar[0], k1.getBonus());
-       k1.totalGajiK();
-        k1.tanggalPegawai[1] = "2016-01-15";
+        k1.totalGajiK();
+        k1.displayMessage1();
+        k1.tanggalPegawai[1] = "2016-01-16";
         k1.jamMasuk[1] = (8);
         k1.jamKeluar[1] = (18.5);
-        k1.dendaPegawai1(k1.jamMasuk[1], k1.jamKeluar[1], k1.getDenda());
-        k1.bonusPegawai1(k1.jamKeluar[1], k1.getBonus());
+        k1.dendaPegawai2(k1.jamMasuk[1], k1.jamKeluar[1], k1.getDenda());
+        k1.bonusPegawai2(k1.jamKeluar[1], k1.getBonus());
         k1.totalGajiK();
-        
+        k1.gajiKeseluruhan();
+        k1.displayMessage2();
+        k1.displayMessage();
+        System.out.println("===============================================================");
+        System.out.println("");
         
         DataKaryawan k2 = new DataKaryawan();
         k2.setNik("102");
@@ -35,10 +40,17 @@ public class MainKaryawan {
         k2.tanggalPegawai[0] = "2016-01-15";
         k2.jamMasuk[0] = (8);
         k2.jamKeluar[0] = (15);
-        k2.dendaPegawai1(k1.jamMasuk[0], k1.jamKeluar[0], k1.getDenda());
-        k2.bonusPegawai1(k1.jamKeluar[0], k1.getBonus());
+        k2.dendaPegawai1(k2.jamMasuk[0], k2.jamKeluar[0], k2.getDenda());
+        k2.bonusPegawai1(k2.jamKeluar[0], k2.getBonus());
+        k2.totalGajiK();
+        k2.displayMessage1();
         k2.tanggalPegawai[1] = null;
         k2.totalGajiK();
+        k2.gajiKeseluruhan();
+        k2.displayMessage2();
+        k2.displayMessage();
+        System.out.println("===============================================================");
+        System.out.println("");
         
         DataKaryawan k3 = new DataKaryawan();
         k3.setNik("103");
@@ -52,9 +64,15 @@ public class MainKaryawan {
         k3.jamKeluar[0] = (17);
         k3.dendaPegawai1(k1.jamMasuk[0], k1.jamKeluar[0], k1.getDenda());
         k3.bonusPegawai1(k1.jamKeluar[0], k1.getBonus());
+        k3.totalGajiK();
+        k3.displayMessage1();
         k3.tanggalPegawai[1] = null;
         k3.totalGajiK();
-        
-        
+        k3.gajiKeseluruhan();
+        k3.displayMessage2();
+        k3.displayMessage();
+        System.out.println("===============================================================");
+        System.out.println("");
+
     }
 }
