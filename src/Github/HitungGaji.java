eddaf13a1bc35e19b92737de5dac761e98d8da26
dwiyan <dwiyan774@gmail.com>
2016-03-09 13:29:49 +0700

    Mencari formula untuk mencari denda di method hitungDenda

--- src/Github/HitungGaji.java
+++ src/Github/HitungGaji.java
@@ -14,21 +14,41 @@ import java.text.SimpleDateFormat;
  */
 public class HitungGaji {
 
+    public static void main(String[] args) {
+        double denda;
+
+    }
+
     public void hitung() {
-        double gaji = 0;
+
+    }
+
+    public double hitungDenda(String awal, String akhir, int ap) {
+        double denda = 0;
         JadwalKaryawan k1 = new JadwalKaryawan();
 
         k1.setJadwal();
-        String a = "00:00";
-        String b = "01:00";
+        String a = "08:00";
+        String b = "17:00";
+        LocalTime a2 = LocalTime.parse(awal);
+        LocalTime b2 = LocalTime.parse(akhir);
         LocalTime a1 = LocalTime.parse(a);
         LocalTime b1 = LocalTime.parse(b);
 
-        
-    }
+        Boolean c = a1.isAfter(a2);
+        Boolean d = b1.isBefore(b2);
+        if (c == true || d == true) {
 
-    public double bonus(double b) {
-        double banyakBonus = 0;
-        return banyakBonus;
+            int waktu1 = a2.getHour() - a1.getHour();
+            int waktu2 = b1.getHour() - b2.getHour();
+
+            denda = (waktu1 + waktu2) * ap;
+        }
+        return denda;
+    }
+    
+    public double hitungBonus(){
+        
+        return 0;
     }
 }
