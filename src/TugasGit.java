a1e959db04985642987adba94073a72f630c3de1
mfnaim <naimfattahp@gmail.com>
2016-03-09 20:24:20 +0700

    Create TugasGit.java
    membuat class baru dan deklarasi

---
+++ src/TugasGit.java
@@ -0,0 +1,12 @@
+package maintugasgit;
+
+public class TugasGit {
+
+    int nik, gaji, tunjangan, bonus, denda;
+    String nama;
+    int g_paijo = 900, g_lulu = 875, g_lala = 1000;
+    int t_paijo = 200, t_lulu = 200, t_lala = 350;
+    int b_paijo = 150, b_lulu = 100, b_lala = 200;
+    int d_paijo = 75, d_lulu = 100, d_lala = 125;
+    int jampaijo = 8, jamlulu = 8, jamlala = 8, jamkpaijo = 20, jamklulu = 20, jamkpaijo2 = 17, jamklala = 17;
+    int jam;
