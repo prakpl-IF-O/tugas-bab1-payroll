63d7f61537811a905c7312c77278b60e6416ef29
suastikaadinata <suastikaadinata97@gmail.com>
2016-03-09 21:35:34 +0700

    Melakukan debug test dan melakukan pengeditan

--- src/Main.java
+++ src/Main.java
@@ -25,6 +25,7 @@ public class Main {
         double pulang = 17.00;
         double[] bonus = new double[3];
         double[] denda = new double[3];
+        double [] total_gaji = new double[4];
         Karyawan k[] = new Karyawan[3];
         Absensi a[] = new Absensi[4];
 
@@ -44,7 +45,7 @@ public class Main {
 
 //        System.out.println(calendar.get(Calendar.DAY_OF_WEEK));
 //        String formatted = dateFormat.format(tanggal);
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 4; i++) {
             if (masuk < a[i].jam_masuk) {
                 denda[i] = k[i].denda * (a[i].jam_masuk - masuk);
             } else {
@@ -63,10 +64,20 @@ public class Main {
             String tanggal1 = dateFormat1.format(calendar.getTime());
             
             if (tanggal1.equals("Saturday")||tanggal1.equals("Sunday")){
-                
-            }
+                bonus[i] = (a[i].jam_pulang-a[i].jam_masuk)*k[i].bonus;
+            }            
         }
-
+        for (int i = 0;i<3;i++){
+            for (int j = 0;j<1;j++){
+                if (k[i].nik==a[j].nik){
+                    total_gaji[i]=((k[i].gaji+k[i].tunjangan+(bonus[i]+bonus[j])
+                            -(denda[i]+denda[j])));
+            }
+                else{
+                    total_gaji[i] = (k[i].gaji+k[i].tunjangan+bonus[i])-denda[i];
+                }
+        }
+            System.out.println(total_gaji);
     }
-
+}
 }
