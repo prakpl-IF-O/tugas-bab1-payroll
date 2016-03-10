4aacf9871014268702b8ed8054ebdaf8f1ae5f01
fakhri wajih <fakhri.wajih@gmail.com>
2016-03-10 07:34:26 +0700

    bismillah

---
+++ src/tugas/Kelas2.java
@@ -0,0 +1,30 @@
+package Tugas;
+
+public class Kelas2 {
+    private String nama;
+    private short nik;
+    private short gaji;
+    private short denda;
+    private short tunjangan;
+    private short bonus;
+    
+    public void setNama(String a){
+        nama=a;
+    }
+    public void setNik(short a){
+        nik=a;
+    }
+    public void setGaji(short a){
+        gaji=a;
+    }
+    public void setDenda(short a){
+        denda=a;
+    }
+    public void setTunjangan(short a){
+        tunjangan=a;
+    }
+    public void setBonus(short a){
+        bonus=a;
+    }
+}
+
